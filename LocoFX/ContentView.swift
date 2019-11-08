e829706112c5c9a6b335f07bad7c7ae50384a579
ChristopherCapps <chris.capps@live.com>
2019-11-08 16:17:08 -0500

    Initial Commit

---
+++ LocoFX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocoFX
+//
+//  Created by Christopher Capps on 11/8/19.
+//  Copyright © 2019 The Peak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
